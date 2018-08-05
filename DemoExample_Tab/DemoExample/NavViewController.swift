ad25f61f159ef0fdafb809b2790f593b2d6d1652
Selin <selin@webinstats.com>
2018-08-05 17:34:50 +0300

    new demo app added (with tab & navbar)

---
+++ DemoExample_Tab/DemoExample/NavViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NavViewController.swift
+//  DemoExample
+//
+//  Created by Fatma Selin Hangişi on 3.08.2018.
+//  Copyright © 2018 Fatma Selin Hangişi. All rights reserved.
+//
+
+import UIKit
+import iOS_wis
+
+class NavViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        let wiso = Webinstats("//demoapb.webinstats.com/","1525","0")
+        var map = [String:String]()
+        map["p"] = "Home Page3"
+        map["_enable_push"] = "1"
+        wiso.execute(view: self, localmap: map)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DemoExample_Tab/DemoExample/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  DemoExample
+//
+//  Created by Fatma Selin Hangişi on 09/10/2017.
+//  Copyright © 2017 Fatma Selin Hangişi. All rights reserved.
+//
+
+import UIKit
+import iOS_wis
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        let wiso = Webinstats("//demoapb.webinstats.com/","1525","0")
+        var map = [String:String]()
+        map["p"] = "Home Page1"
+        map["_enable_push"] = "1"
+        wiso.execute(view: self, localmap: map)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
